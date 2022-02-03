9ce7feb48e4292fd601a823a0f20abc9760b54d0
PIDAMI <pop-dod@rambler.ru>
2022-02-03 21:13:46 +0300

    all but division tests implemented, no profiles or manually added maven plugins

---
+++ src/test/java/BaseCalculatorOperTest.java
@@ -0,0 +1,13 @@
+import com.epam.tat.module4.Calculator;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseCalculatorOperTest {
+    public Calculator calculator;
+    public final double DOUBLE_OFFSET = 1e-5;
+
+    @BeforeMethod
+    public void setUp(){
+        calculator = new Calculator();
+    }
+
+}
---
+++ src/test/java/CalculatorSubtractTest.java
@@ -0,0 +1,63 @@
+import com.epam.tat.module4.Calculator;
+import org.assertj.core.api.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorSubtractTest extends BaseCalculatorOperTest{
+
+    @DataProvider(name = "dataForSubtractLong")
+    public Object[][] getDataForSubtractLong(){
+        Object[][] obj = {
+                { 7, 12, -5 },
+                {20, 1, 19},
+                { 9, 0, 0 },
+                { -4, -64, 60 },
+                {Integer.MAX_VALUE, -1, 2_147_483_648L},
+                {Long.MIN_VALUE, -Long.MAX_VALUE, 0},
+        };
+        return obj;
+    }
+
+    @DataProvider(name = "dataForSubtractDouble")
+    public Object[][] getDataForSubtractDouble(){
+        Object[][] obj = {
+                { 7., 12., -5. },
+                {20., 1., 19.},
+                { 9., 0., 0. },
+                { -4.12, -2., -2.12 },
+                {Integer.MAX_VALUE, -1., 2_147_483_648.}
+        };
+        return obj;
+    }
+
+    @Test(dataProvider = "dataForSubtractLong")
+    public void testSubtractLong(long x, long y, long expected) {
+        long actual = calculator.sub(x,y);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test(dataProvider = "dataForSubtractLong")
+    public void testSubtractAnticommutativityLong
+            (long x, long y, long expected) {
+        long oneWay = calculator.sub(x,y);
+        long otherWay = calculator.sub(y,x);
+        assertThat(oneWay).isEqualTo(-otherWay);
+    }
+
+    @Test(dataProvider = "dataForSubtractDouble")
+    public void testSubtractDouble(double x, double y, double expected) {
+        double actual = calculator.sub(x,y);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test(dataProvider = "dataForSubtractDouble")
+    public void testSubtractAnticommutativityDouble
+            (double x, double y, double expected) {
+        double oneWay = calculator.sub(x,y);
+        double otherWay = calculator.sub(y,x);
+        assertThat(oneWay).isEqualTo(-otherWay);
+    }
+
+}
