8e85a5a8b0a1fab903abae2cd701366e07a49ef5
PIDAMI <pop-dod@rambler.ru>
2022-02-22 03:44:19 +0300

    fixed attachment bug

--- src/test/java/com/epam/tc/hw4/listeners/AttachmentUtils.java
+++ src/test/java/com/epam/tc/hw4/listeners/AttachmentUtils.java
@@ -2,10 +2,10 @@ package com.epam.tc.hw4.listeners;
 
 import io.qameta.allure.Attachment;
 
-
 public class AttachmentUtils {
     @Attachment(type = "image/png", value = "Try to use name {attachmentName}")
     public static byte[] attachPngImage(String attachmentName, byte[] source) {
         return source;
     }
+
 }
--- src/test/java/com/epam/tc/hw4/listeners/ScreenshotListener.java
+++ src/test/java/com/epam/tc/hw4/listeners/ScreenshotListener.java
@@ -14,6 +14,5 @@ public class ScreenshotListener implements ITestListener {
         Object driver = result.getTestContext().getAttribute("driver");
         byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
         AttachmentUtils.attachPngImage("Screenshot on failure", screenshot);
-
     }
 }
